da068f04080813140ad5cff7f41807201736a3fc
Taggic <taggic@t-online.de>
2012-04-25 17:48:17 +0200

    initial version
    Signed-off-by: Taggic <taggic@t-online.de>

---
+++ lang/de/lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * english language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author Taggic <taggic@t-online.de>
+ */
+$lang['admin_removeold']    = 'Remove outdated files';
+$lang['p_include']          = 'Bitte kopieren sie die Liste der veralteten Datein in folgendes Textfeld.';
+$lang['i_choose']           = 'Ein leeres Eingabefeld hat keine Auswirkung auf Ihr System.<br />';
+$lang['i_legend']           = 'Inhalt von <u><b><a href="http://www.dokuwiki.org/install:upgrade#files_to_remove" target="_blank">removeold</a></b></u> - hier einfügen';
+$lang['i_dryrun']           = 'Simulation';
+$lang['btn_start']          = 'Start';
+$lang['removeold_willmsg']  = 'SIMULATION: Die Dateien würden wie folgt verarbeitet.';
+$lang['removeold_delmsg']   = 'Die Dateien wurden nun <u><b>final</b></u> wie folgt verarbeitet.';
+$lang['removeold_summary']  = 'Zusammenfassung';
+$lang['summary_option']     = 'Nur Zusammenfassung anzeigen !';
+$lang['cache_option']       = 'Cache komplett löschen';
+$lang['not_found']          = 'Datei nicht gefunden';
+$lang['exists']             = 'Datei existiert';
+$lang['deleted']            = 'Datei gelöscht';
+$lang['delError']           = '<span style="color:red;">! FEHLER ! </span>';
+$lang['erased']             = 'Cache gelöscht';
+$lang['ro_err_msg']         = '<b>LOG SCHREIBFEHLER !</b> Dem Log-File können keine Einträge hinzugefügt werden.';
