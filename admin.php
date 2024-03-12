d038d001032b8d51102f5506e7d3c33d59774b7d
Andreas Gohr <andi@splitbrain.org>
2024-03-12 11:19:56 +0100

    initial cleanup

--- admin.php
+++ admin.php
@@ -5,15 +5,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     Taggic <taggic@t-online.de>
  */
-/******************************************************************************/
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
 
-/******************************************************************************/
-/**
- * All DokuWiki plugins to extend the admin function
- * need to inherit from this class
- */
 class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
     /**
      * return some info
@@ -21,21 +13,21 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
     function getInfo(){
         return confToHash(dirname(__FILE__).'/plugin.info.txt');
     }
-/******************************************************************************/
+
     /**
      * return prompt for admin menu
      */
     function getMenuText($language) {
         return $this->getLang('admin_removeold');
     }
-/******************************************************************************/
+
     /**
      * return sort order for position in admin menu
      */
     function getMenuSort() {
         return 20;
     }
-/******************************************************************************/
+
     /**
      * handle user request
      *
@@ -46,7 +38,7 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
     function handle() {
         global $ID;
     }
-/******************************************************************************/
+
     /**
      * removeold Output function
      *
@@ -72,17 +64,17 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
         $filelist       = $_REQUEST['removeold_w'];
         $dryrun         = $_REQUEST['dryrun'];
         $summary_option = $_REQUEST['summary_only'];
-        
+
         // language given?
         if ($filelist!=false) {
             if (($dryrun==true) && ($summary_option==false)) {
-              echo '<br /><div class="level4"><strong><div class="it__standard_feedback">'.$this->getLang('removeold_willmsg').'</div></strong><br />';              
+              echo '<br /><div class="level4"><strong><div class="it__standard_feedback">'.$this->getLang('removeold_willmsg').'</div></strong><br />';
             }
             elseif ($summary_option==false) {
               echo '<br /><div class="level4"><strong><div class="it__standard_feedback">'.$this->getLang('removeold_delmsg').'</div></strong><br />';
             }
-            
-            $this->_list_removeold_files($filelist, $dryrun, $summary_option);        
+
+            $this->_list_removeold_files($filelist, $dryrun, $summary_option);
         }
         echo '</div>'.NL;
         echo '<div class="footnotes"><div class="fn">'.NL;
@@ -92,11 +84,11 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
 
         echo '</div>'.NL;
     }
-/******************************************************************************/
+
     /**
      * Display the form with input control to let the user specify
      * the files to be deleted
-     *     
+     *
      * @author  Taggic <taggic@t-online.de>
      */
     function _html_uinput(){
@@ -135,15 +127,14 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
           msg("File not found: ".$deleted_files,-1);
         }
     }
-    
-/******************************************************************************/
+
   /**
    * This function will loop through the given files.
    * It checks if the entry is not empty or comment and if such a file does exist.
-   * If the file does exist then it will be deleted. The result will be stored 
-   * into the filelist array for user feddback.         
+   * If the file does exist then it will be deleted. The result will be stored
+   * into the filelist array for user feddback.
    */
-    function _list_removeold_files($afilelist, $dryrun, $summary_option){ 
+    function _list_removeold_files($afilelist, $dryrun, $summary_option){
         # statistic counters
         # $empty          = count empty lines
         # $comments       = count comments
@@ -151,10 +142,10 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
         # $files_notFound = count missing files
         # $files_deleted  = count deleted files
         # $files_delError = count files impossible to delete
-        
+
         $filelist = explode(chr(10),$afilelist);
         echo '<div class="level4" id="removeold__input"><p>'.NL;
-        
+
         foreach($filelist as &$file) {
             $file = trim($file);
             # check if item is empty   => continue, do nothing
@@ -162,11 +153,11 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
             # check if item is comment => continue, do nothing
             if(stripos($file,"#")===0) { $comments++; continue; }
             # check if file does exist
-            if(file_exists(DOKU_INC.$file)===true) { 
+            if(file_exists(DOKU_INC.$file)===true) {
                 # delete file (except on dryrun)
                 $files_found++;
                 $result = $this->getLang('exists');
-                                
+
                 if(($dryrun==true) && ($this->is__writable(DOKU_INC.$file)==true)) {
                   $result = $this->getLang('deleted');
                   $files_deleted++;
@@ -175,7 +166,7 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
                   $result = $this->getLang('delError');
                   $files_delError++;
                 }
-                
+
                 if($dryrun==false) {
                     $result = unlink(DOKU_INC.$file);
                     if($result === true) {
@@ -206,33 +197,37 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
         echo '<div class="level2">'.NL;
         echo '<table><tr><td>'.$this->getLang('exists').': </td><td>'.$files_found.'</td></tr>'.NL;
         echo '<tr><td>'.$this->getLang('deleted').': </td><td>'.$files_deleted.'</td></tr>'.NL;
-        echo '<tr><td>'.$this->getLang('delError').' </td><td>'.$files_delError.'</td></tr></table>'.NL;        
-        
-        echo '</div><br />'.NL;        
+        echo '<tr><td>'.$this->getLang('delError').' </td><td>'.$files_delError.'</td></tr></table>'.NL;
+
+        echo '</div><br />'.NL;
     }
-/******************************************************************************/
-# Since looks like the Windows ACLs bug "wont fix" 
-# (see http://bugs.php.net/bug.php?id=27609) 
-# alternative function proposed on php.net:
+
+    /**
+     * Since looks like the Windows ACLs bug "wont fix"
+     * (see http://bugs.php.net/bug.php?id=27609)
+     * alternative function proposed on php.net:
+     */
     function is__writable($path) {
         if ($path{strlen($path)-1}=='/')
              return is__writable($path.uniqid(mt_rand()).'.tmp');
-         
+
         if (file_exists($path)) {
              if (!($f = @fopen($path, 'r+')))
                  return false;
              fclose($f);
              return true;
          }
-         
+
         if (!($f = @fopen($path, 'w')))
              return false;
          fclose($f);
          unlink($path);
          return true;
      }
-/******************************************************************************/ 
-/* logging of deleted files and deletion errors                               */
+
+    /**
+     *  logging of deleted files and deletion errors
+     */
     function __removeold_logging($file, $result) {
       global $conf;
       $timestamp = date('d/M/Y G:i:s');
@@ -240,15 +235,15 @@ class admin_plugin_removeold extends DokuWiki_Admin_Plugin {
         $log_file = DOKU_INC."/".$conf["savedir"].'/tmp/removeold.log';
       }
       else $log_file = $conf["savedir"].'/tmp/removeold.log';
-      
+
       $record = "[".$timestamp."]".chr(9).$result.chr(9).chr(9).$file.chr(10);
-      
+
       // Save logging records
       $fh = fopen($log_file, 'a+');
       if (!fwrite($fh, $record)) {
         echo "<span style=\"color:red;\">".$this->getLang('ro_err_msg')."</span><br />".NL;
       }
       fclose($fh);
-    } 
-/******************************************************************************/ 
+    }
+
 }
