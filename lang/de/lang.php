6f671a58d30894ee96bd4d44575b1420f74089b5
Andreas Gohr <andi@splitbrain.org>
2024-03-12 13:29:24 +0100

    remove unused language strings

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -6,19 +6,5 @@
  * @author Taggic <taggic@t-online.de>
  */
 $lang['menu']    = 'Remove outdated files';
-$lang['p_include']          = 'Bitte kopieren sie die Liste der veralteten Datein in folgendes Textfeld.';
-$lang['i_choose']           = 'Ein leeres Eingabefeld hat keine Auswirkung auf Ihr System.<br />';
-$lang['i_legend']           = 'Inhalt von <u><b><a href="https://github.com/splitbrain/dokuwiki/raw/stable/data/deleted.files" target="_blank">removeold</a></b></u> - hier einfügen';
-$lang['i_dryrun']           = 'Simulation';
-$lang['btn_start']          = 'Start';
 $lang['removeold_willmsg']  = 'SIMULATION: Die Dateien würden wie folgt verarbeitet.';
 $lang['removeold_delmsg']   = 'Die Dateien wurden nun <u><b>final</b></u> wie folgt verarbeitet.';
-$lang['removeold_summary']  = 'Zusammenfassung';
-$lang['summary_option']     = 'Nur Zusammenfassung anzeigen !';
-$lang['cache_option']       = 'Cache komplett löschen';
-$lang['not_found']          = 'Datei nicht gefunden';
-$lang['exists']             = 'Datei existiert';
-$lang['deleted']            = 'Datei gelöscht';
-$lang['delError']           = '<span style="color:red;">! FEHLER ! </span>';
-$lang['erased']             = 'Cache gelöscht';
-$lang['ro_err_msg']         = '<b>LOG SCHREIBFEHLER !</b> Dem Log-File können keine Einträge hinzugefügt werden.';
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -6,22 +6,8 @@
  * @author Taggic <taggic@t-online.de>
  */
 $lang['menu']    = 'Remove outdated files';
-$lang['p_include']          = 'Please copy the list of outdated files into the textarea.';
-$lang['i_choose']           = 'An empty input field will not cause any effect to prevent misuse/mistakes.<br />';
-$lang['i_legend']           = 'Insert the <u><b><a href="https://github.com/splitbrain/dokuwiki/raw/stable/data/deleted.files" target="_blank">removeold</a></b></u> - file content here';
-$lang['i_dryrun']           = 'simulate only';
-$lang['btn_start']          = 'Start';
 $lang['removeold_willmsg']  = 'SIMULATION: The files would be processed as follows.';
 $lang['removeold_delmsg']   = 'The files are <u><b>finally</b></u> processed as follows.';
-$lang['removeold_summary']  = 'Summary';
-$lang['summary_option']     = 'Show summary only !';
-$lang['cache_option']       = 'Erase cache completely';
-$lang['not_found']          = 'File not found';
-$lang['exists']             = 'Files exist';
-$lang['deleted']            = 'Files deleted';
-$lang['delError']           = '<span style="color:red;">! ERROR ! </span>';
-$lang['erased']             = 'Cache erased';
-$lang['ro_err_msg']         = '<b>LOG WRITE ERROR !</b> Can\'t write to the log file.';
 
 $lang['button']             = 'Delete obsolete files!';
 $lang['rm_notfound']        = 'Not found: %s';
